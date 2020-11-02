11b9e913f0b321eab956c428a79da4f760453dd1
zzh1650998665 <1650998665@qq.com>
2020-11-02 10:43:23 +0800

    更新

---
+++ src/main/java/day1/workquene/Customer2.java
@@ -0,0 +1,30 @@
+package day1.workquene;
+
+import com.rabbitmq.client.*;
+import day1.utils.RabbitMQUtils;
+
+import java.io.IOException;
+
+public class Customer2 {
+    public static void main(String[] args) throws IOException {
+
+        //获取连接
+        Connection connection = RabbitMQUtils.getConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.basicQos(1);
+        channel.queueDeclare("work",true,false,true,null);
+
+        channel.basicConsume("work",false,new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者-2: "+new String(body));
+                //手动确认  参数1:手动确认消息标识  参数2:false 每次确认一个
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        });
+
+        
+
+    }
+}
